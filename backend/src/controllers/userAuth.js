f455c5b68c46a44ebaa2728198a4eebd85062490
attriashwani09 <attriashwani09@gmail.com>
2026-07-12 22:58:21 +0530

    Minor change : userAuth Fromatted

--- backend/src/controllers/userAuth.js
+++ backend/src/controllers/userAuth.js
@@ -4,63 +4,55 @@ const jwt = require("jsonwebtoken") ;
 const validate = require("../utils/validate") ;
 
 
-const register = async ( req , res ) =>{
+const register = async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
 
-    try{
-    const { emailId , password } = req.body ;
+    validate(req.body);
 
-    validate( req.body ) ; 
+    req.body.password = await bcrypt.hash(password, 10);
 
-    req.body.password = await bcrypt.hash( password , 10 ) ; 
+    const user = await User.create(req.body); // create user in DB
 
-    const user = await User.create( req.body ) ;  // create user in DB 
+    const token = await jwt.sign( { _id: user._id, emailId: user.emailId }, process.env.JWT_KEY, { expiresIn: 3600 } );
 
-    const token = await jwt.sign({ _id : user._id , emailId : user.emailId } , process.env.JWT_KEY  , {expiresIn : 3600 }) ;
+    res.cookie("token", token, { maxAge: 3600 * 1000 }); // Here we multiplied by 1000 because maxAge uses milisecond value
 
-    res.cookie("token" , token , {maxAge : 3600 * 1000 }) ; // Here we multiplied by 1000 because maxAge uses milisecond value
+    res.status(201).send("User created successfully ..... ");
+  } 
+  catch (err) {
+    res.send("Err : " + err.message);
+  }
+};
 
-    res.status( 201 ).send("User created successfully ..... ") ; 
+const login = async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
 
-    } 
-    catch( err ){
-        res.send("Err : " + err.message ) ;
+    if (!emailId) {
+      throw new Error("Please enter a valid email address ");
     }
-    
-}  
 
-
-const login = async ( req , res ) =>{ 
-
-    try{
-
-    const { emailId , password } = req.body ;
-
-    if( !emailId ){
-        throw new Error("Please enter a valid email address ") ;
-    } 
-
-    if( !password ){
-        throw new Error("Please enter your password ") ;
-    }  
-
-    const  user = await User.findOne({ emailId : emailId }) ; 
-
-    const match = bcrypt.compare( user.password , password ) ;
-
-    if( !match )
-        throw new Error("Invalid credentials ...... ") ;
-
-    const token = jwt.sign({ _id : user._id , emailId : user.emailId }  , process.env.JWT_KEY  , {expiresIn : 3600 }) ;
-
-    res.cookie( "token" , token , { maxAge : 3600 * 1000 }) ;
-
-    res.status(200).send("Login Succesfully ....... " + user.firstName ) ;
-
-    } 
-    catch( err ){
-        res.send("Err : " + err.message ) ;
+    if (!password) {
+      throw new Error("Please enter your password ");
     }
-}
+
+    const user = await User.findOne({ emailId: emailId });
+
+    const match = bcrypt.compare(user.password, password);
+
+    if (!match) throw new Error("Invalid credentials ...... ");
+
+    const token = jwt.sign( { _id: user._id, emailId: user.emailId }, process.env.JWT_KEY, { expiresIn: 3600 } );
+
+    res.cookie("token", token, { maxAge: 3600 * 1000 });
+
+    res.status(200).send("Login Succesfully ....... " + user.firstName);
+  } 
+  catch (err) {
+    res.send("Err : " + err.message);
+  }
+};
 
 
 
